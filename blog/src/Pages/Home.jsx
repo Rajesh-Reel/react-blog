bd2ccbd0da666e5d17ce2dee0c8e2135a3a3042e
Rajesh-Reel <rajreel2@hotmail.com>
2023-04-16 12:28:17 +0100

    added a click event function to console log a message

--- blog/src/Pages/Home.jsx
+++ blog/src/Pages/Home.jsx
@@ -2,10 +2,15 @@ function Home() {
     const mainTitle = "Welcome to the react blog";
     const likes = 76;
 
+    function handleClick() {
+        console.log("clicked");
+    }
+
     return(
         <div className="home">
             <h1>{mainTitle}</h1>
             <p>Post liked by {likes} people</p>
+            <button onClick={handleClick}>Clicky</button>
         </div>
     )
 }
