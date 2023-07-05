e41b115f295675139d0e6c4e583c239440f16811
Rajesh-Reel <rajreel2@hotmail.com>
2023-07-05 12:09:22 +0100

    added use state and name in p tag

--- blog/src/Pages/Home.jsx
+++ blog/src/Pages/Home.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 function Home() {
     const mainTitle = "Welcome to the react blog";
     const likes = 76;
 
+    const [name, setName] = useState("Rajesh");
+
     function handleClick() {
         console.log("clicked");
+        setName("Not Rajesh")
     }
 
     return(
         <div className="home">
             <h1>{mainTitle}</h1>
             <p>Post liked by {likes} people</p>
+            <p>{ name }</p>
             <button onClick={handleClick}>Clicky</button>
         </div>
     )
